7070dbaf6a5d360847e990000f518bd4bee0f284
TypicalSpider98 <typicalspider98@gmail.com>
2024-10-09 22:48:34 +1300

    Update: Menu window supports dragging

--- src/ui/ui.cpp
+++ src/ui/ui.cpp
@@ -35,10 +35,16 @@ void UI::render()
 	ImGui::NewFrame();
 
 	// --- set up window  & location to top-left corner---
-	ImGui::SetNextWindowPos(ImVec2(CGRA350Constants::DEFAULT_WINDOW_WIDTH - CGRA350Constants::DEFAULT_IMGUI_WIDTH, 0));
-	ImGui::SetNextWindowSize(ImVec2(CGRA350Constants::DEFAULT_IMGUI_WIDTH, CGRA350Constants::DEFAULT_WINDOW_HEIGHT));
-	ImGuiWindowFlags window_flags = ImGuiWindowFlags_NoMove;
-	ImGui::Begin("CGRA350", nullptr, window_flags);
+	//ImGui::SetNextWindowPos(ImVec2(CGRA350Constants::DEFAULT_WINDOW_WIDTH - CGRA350Constants::DEFAULT_IMGUI_WIDTH, 0));
+	//ImGui::SetNextWindowSize(ImVec2(CGRA350Constants::DEFAULT_IMGUI_WIDTH, CGRA350Constants::DEFAULT_WINDOW_HEIGHT));
+	
+	// This is only called during initial setup.
+	ImGui::SetNextWindowPos(ImVec2(CGRA350Constants::DEFAULT_WINDOW_WIDTH - CGRA350Constants::DEFAULT_IMGUI_WIDTH, 0), ImGuiCond_FirstUseEver);
+	ImGui::SetNextWindowSize(ImVec2(CGRA350Constants::DEFAULT_IMGUI_WIDTH, CGRA350Constants::DEFAULT_WINDOW_HEIGHT), ImGuiCond_FirstUseEver);
+
+	//ImGuiWindowFlags window_flags = ImGuiWindowFlags_NoMove;
+	//ImGui::Begin("CGRA350", nullptr, window_flags);
+	ImGui::Begin("CGRA350");
 
 	// --- set up widgets ---
 		
